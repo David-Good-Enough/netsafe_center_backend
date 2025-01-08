7a2985a1e6be3bf8004d2e9ae9aaa11820ed8c7a
David-Good-Enough <124247495+David-Good-Enough@users.noreply.github.com>
2025-01-08 09:26:50 +0100

    {~}

---
+++ routes/Login.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const pool = require('../config/db');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE identifiant = $1', [username]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ success: false, message: 'Utilisateur non trouvé' });
+        }
+
+        const user = result.rows[0];
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            return res.status(401).json({ success: false, message: 'Mot de passe incorrect' });
+        }
+
+        const token = jwt.sign({ userId: user.id, username: user.identifiant }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ success: true, token });
+        }catch(err){
+        console.error(err);
+        res.status(500).json({ success: false, message: 'Erreur serveur' });
+    }
+});
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const TestRoute = require("./routes/Test");
+const LoginRoute = require("./routes/Login");
 
 const app = express();
 const port = process.env.PORT
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/Test', TestRoute);
+app.use('/Login', LoginRoute);
 
 
 app.listen(port, () => {
